2cf74561ed44a9b5591796b535bd377c05545c44
Aben1002 <92834190+ThanhTung2003@users.noreply.github.com>
2024-06-15 21:22:11 +0700

    hoàn thành UI cho danh sách đồ ăn

--- lib/screen/food_list_screen.dart
+++ lib/screen/food_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_animated/auto_animated.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:oderfoodapp_flutter/const/const.dart';
@@ -16,71 +17,100 @@ class FoodListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            categoryStateController.selectedCategory.value.name,
-            style: GoogleFonts.poppins(color: Colors.black),
-          ),
-          elevation: 10,
-          backgroundColor: const Color(0xFFA696D6),
-          foregroundColor: const Color(0xFF644AB5),
-          iconTheme: const IconThemeData(color: Colors.black),
+      appBar: AppBar(
+        title: Text(
+          categoryStateController.selectedCategory.value.name,
+          style: GoogleFonts.poppins(color: Colors.black),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: LiveList(
-                showItemDuration: const Duration(milliseconds: 300),
-                showItemInterval: const Duration(milliseconds: 300),
-                reAnimateOnVisibility: true,
-                scrollDirection: Axis.vertical,
-                itemCount:
-                    categoryStateController.selectedCategory.value.foods.length,
-                itemBuilder: animationItemBuilder((index) => InkWell(
-                    onTap: () {},
-                    child: SizedBox(
-                      height: MediaQuery.of(context).size.height / 6 * 2,
-                      child: Card(
-                        semanticContainer: true,
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        child: Stack(
-                          children: [
-                            CachedNetworkImage(
-                              imageUrl: categoryStateController
-                                  .selectedCategory.value.foods[index].image,
-                              fit: BoxFit.cover,
-                              errorWidget: (context, url, error) =>
-                                  const Center(
-                                child: Icon(Icons.image),
-                              ),
-                              progressIndicatorBuilder:
-                                  (context, url, downloadProgress) =>
-                                      const Center(
-                                          child: CircularProgressIndicator()),
-                            ),
-                            Container(
-                              color: const Color(
-                                  COLOR_OVERPLAY), // Applying ARGB overlay color
-                            ),
-                            Center(
-                              child: Text(
-                                categoryStateController
-                                    .selectedCategory.value.foods[index].name,
-                                textAlign: TextAlign.center,
-                                style: GoogleFonts.poppins(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ],
+        elevation: 10,
+        backgroundColor: const Color(0xFFA696D6),
+        foregroundColor: const Color(0xFF644AB5),
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: LiveList(
+              showItemDuration: const Duration(milliseconds: 300),
+              showItemInterval: const Duration(milliseconds: 300),
+              reAnimateOnVisibility: true,
+              scrollDirection: Axis.vertical,
+              itemCount: categoryStateController.selectedCategory.value.foods.length,
+              itemBuilder: animationItemBuilder((index) => InkWell(
+                onTap: () {},
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: Card(
+                    semanticContainer: true,
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: Stack(
+                      children: [
+                        CachedNetworkImage(
+                          imageUrl: categoryStateController.selectedCategory.value.foods[index].image,
+                          fit: BoxFit.cover,
+                          errorWidget: (context, url, error) => const Center(
+                            child: Icon(Icons.image),
+                          ),
+                          progressIndicatorBuilder: (context, url, downloadProgress) =>
+                              const Center(child: CircularProgressIndicator()),
                         ),
-                      ),
-                    ))),
-              ),
-            )
-          ],
-        ));
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            color: const Color(COLOR_OVERPLAY),
+                            width: double.infinity,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 10, bottom: 10),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    categoryStateController.selectedCategory.value.foods[index].name,
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Giá: ${categoryStateController.selectedCategory.value.foods[index].price} vnđ',
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      IconButton(onPressed: (){},
+                                       icon: const Icon(
+                                        Icons.favorite_border,
+                                        color: Colors.white,
+                                       )),
+                                      const SizedBox(width:50,), 
+                                      IconButton(onPressed: (){},
+                                       icon: const Icon(
+                                        Icons.add_shopping_cart_outlined,
+                                        color: Colors.white,
+                                       )),
+                                    ],
+
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
