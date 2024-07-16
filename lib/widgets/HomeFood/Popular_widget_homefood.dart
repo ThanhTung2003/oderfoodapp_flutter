eec4391181f20c4b093d952bb73a74b51ae5ac79
ThanhTung2003 <tungb100203@gmail.com>
2024-07-16 16:19:32 +0700

    fix 16/7

--- lib/widgets/HomeFood/Popular_widget_homefood.dart
+++ lib/widgets/HomeFood/Popular_widget_homefood.dart
@@ -30,7 +30,7 @@ class popular_widget_homefood extends StatelessWidget {
                 width: 150,
                 height: 205,
                 decoration: BoxDecoration(
-                    color: const Color(0xFFD9D9D9),
+                    color: const Color.fromARGB(255, 242, 237, 237),
                     borderRadius: BorderRadius.circular(10),
                     boxShadow: [
                       BoxShadow(
@@ -96,7 +96,7 @@ class popular_widget_homefood extends StatelessWidget {
                 width: 150,
                 height: 205,
                 decoration: BoxDecoration(
-                    color: const Color(0xFFD9D9D9),
+                    color: const Color.fromARGB(255, 242, 237, 237),
                     borderRadius: BorderRadius.circular(10),
                     boxShadow: [
                       BoxShadow(
@@ -160,7 +160,7 @@ class popular_widget_homefood extends StatelessWidget {
                 width: 150,
                 height: 205,
                 decoration: BoxDecoration(
-                    color: const Color(0xFFD9D9D9),
+                    color: const Color.fromARGB(255, 242, 237, 237),
                     borderRadius: BorderRadius.circular(10),
                     boxShadow: [
                       BoxShadow(
@@ -224,7 +224,7 @@ class popular_widget_homefood extends StatelessWidget {
                 width: 150,
                 height: 205,
                 decoration: BoxDecoration(
-                    color: const Color(0xFFD9D9D9),
+                    color: const Color.fromARGB(255, 242, 237, 237),
                     borderRadius: BorderRadius.circular(10),
                     boxShadow: [
                       BoxShadow(
@@ -288,7 +288,7 @@ class popular_widget_homefood extends StatelessWidget {
                 width: 150,
                 height: 205,
                 decoration: BoxDecoration(
-                    color: const Color(0xFFD9D9D9),
+                    color: const Color.fromARGB(255, 242, 237, 237),
                     borderRadius: BorderRadius.circular(10),
                     boxShadow: [
                       BoxShadow(
